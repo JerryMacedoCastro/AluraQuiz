d81d803205edba9487a8049aae961a224c4248ca
Jerry Macedo Castro <leom551@hotmail.com>
2021-01-30 10:27:49 -0300

    api que retorna o json

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,6 +1,17 @@
 import data from '../../db.json';
 
-const getData = (_request, response) => {
+const getData = (request, response) => {
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
+  }
+
+  response.setHeader('Access-Control-Allow-Credentials', true);
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, OPTIONS, PATCH, DELETE, POST, PUT'
+  );
   return response.json(data);
 };
 
